fe29e0bfa809ef88ffc3a49ecc5b1920f82c37f3
Armando Bernal Montoya <benny.arm14@gmail.com>
2018-02-01 10:38:20 -0600

    Primera versión sin modificar.

---
+++ AOC_Control_Activos/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AOC_Control_Activos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AOC_Control_Activos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AOC_Control_Activos.MvcApplication" Language="C#" %>
